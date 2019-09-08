724836b00bc816ed831b07d68a332de0f715d143
Su Yatanar <suyatanar86@gmail.com>
2019-09-08 13:42:39 +0800

    Setup plugin

---
+++ dummy-data.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Import Dummy Data
+ * Plugin URI: #
+ * Description: Import dummy data for post type
+ * Version: 1.0
+ * Author: Su Yatanar
+ * Author URI: #
+ */
+
+require_once(ABSPATH . 'wp-config.php'); 
+require_once(ABSPATH . 'wp-includes/wp-db.php'); 
+require_once(ABSPATH . 'wp-admin/includes/taxonomy.php'); 
+
+function import_data_register_settings() {
+	add_option( 'import_data_option_name', 'This is my option value.');
+	register_setting( 'myplugin_options_group', 'import_data_option_name', 'myplugin_callback' );
+}
+add_action( 'admin_init', 'import_data_register_settings' );
+function import_data_register_options_page() {
+	add_options_page('Page Title', 'Import Dummy', 'manage_options', 'import_data', 'import_data_options_page');
+}
+add_action('admin_menu', 'import_data_register_options_page');
